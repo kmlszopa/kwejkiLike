742646e3ce2145ae33b56e372571cec7be8a89e5
Kamil Szopaan <kmlszopa@gmail.com>
2016-12-09 15:47:07 +0100

    remove unused import

--- src/main/java/pl/kamilszopa/controller/GifController.java
+++ src/main/java/pl/kamilszopa/controller/GifController.java
@@ -1,11 +1,12 @@
 package pl.kamilszopa.controller;
 
-import org.jcp.xml.dsig.internal.MacOutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.kamilszopa.data.GifRepository;
 import pl.kamilszopa.model.Gif;
 
